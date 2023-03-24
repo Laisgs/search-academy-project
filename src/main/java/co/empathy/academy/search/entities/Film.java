62e127c73f34b9672d2e470eca4816a681b113fa
laisg <laisGarciaSuarez@gmail.com>
2023-03-24 13:52:50 +0100

    Modelo para película y titulo creado

---
+++ src/main/java/co/empathy/academy/search/entities/Film.java
@@ -0,0 +1,67 @@
+package co.empathy.academy.search.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Film {
+    private List<Title> titles = new ArrayList<>();
+    private String type;
+    private String primaryTitle;
+    private String originalTitle;
+    private int startYear;
+    private int endYear;
+    private int runtimeMinutes;
+    private List<String> genres = new ArrayList<>();;
+    private List<String> directorsIds = new ArrayList<>();;
+    private List<String> writersIds = new ArrayList<>();;
+    private double averageRating;
+    private int numberOfVotes;
+
+    public void addTitle(Title title){
+        titles.add(title);
+    }
+
+    public void addGenre(String genre){
+        genres.add(genre);
+    }
+
+    public void addDirectorId(String id){
+        directorsIds.add(id);
+    }
+
+    public void addWriterId(String id){
+        writersIds.add(id);
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setPrimaryTitle(String primaryTitle) {
+        this.primaryTitle = primaryTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public void setStartYear(int startYear) {
+        this.startYear = startYear;
+    }
+
+    public void setEndYear(int endYear) {
+        this.endYear = endYear;
+    }
+
+    public void setRuntimeMinutes(int runtimeMinutes) {
+        this.runtimeMinutes = runtimeMinutes;
+    }
+
+    public void setAverageRating(double averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    public void setNumberOfVotes(int numberOfVotes) {
+        this.numberOfVotes = numberOfVotes;
+    }
+}
---
+++ src/main/java/co/empathy/academy/search/entities/Title.java
@@ -0,0 +1,14 @@
+package co.empathy.academy.search.entities;
+
+import java.util.List;
+
+public class Title {
+    private String id;
+    private String title;
+    private String region;
+    private String language;
+    private List<String> types;
+    private List<String> attributes;
+    private boolean isOriginalTitle;
+
+}
