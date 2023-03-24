0d571ab6f98b0879bd8004a42be8a5ed398f27e3
laisg <laisGarciaSuarez@gmail.com>
2023-03-24 14:05:08 +0100

    Modelo para persona añadido pero sin uso claro aun

--- src/main/java/co/empathy/academy/search/entities/Person.java
+++ src/main/java/co/empathy/academy/search/entities/Person.java
@@ -1,4 +1,32 @@
 package co.empathy.academy.search.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
+    private String name;
+    private int birthYear;
+    private int deathYear;
+    private List<String> primaryProfessions = new ArrayList<>();
+    private List<String> knownForTitles = new ArrayList<>();
+
+    public void addProfession(String profssion){
+        primaryProfessions.add(profssion);
+    }
+
+    public void addFilmId(String id){
+        knownForTitles.add(id);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public void setDeathYear(int deathYear) {
+        this.deathYear = deathYear;
+    }
 }
--- src/main/java/co/empathy/academy/search/entities/Title.java
+++ src/main/java/co/empathy/academy/search/entities/Title.java
@@ -1,5 +1,6 @@
 package co.empathy.academy.search.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Title {
@@ -7,8 +8,35 @@ public class Title {
     private String title;
     private String region;
     private String language;
-    private List<String> types;
-    private List<String> attributes;
+    private List<String> types = new ArrayList<>();
+    private List<String> attributes = new ArrayList<>();
     private boolean isOriginalTitle;
 
+    public void addType(String type){
+        types.add(type);
+    }
+
+    public void addAttribute(String attribute){
+        attributes.add(attribute);
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void setIsOriginalTitle() {
+        isOriginalTitle = true;
+    }
 }
